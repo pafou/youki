9793ff349716c99b26dba621140b303b428c8c8b
pafou <pfoucher@gmail.com>
2017-01-28 09:48:36 +0100

    ajout de bon appetit

---
+++ bonappetit.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "Bon appetit l'ami !"
--- lanceur.bash
+++ lanceur.bash
@@ -5,5 +5,8 @@ echo " Je suis un lanceur"
 echo "==> lancement de bonjour.bash"
 ./bonjour.bash
 
+echo "==> lancement de bonappetit.bash"
+./bonappetit.bash
+
 echo "==> lancement de aurevoir.bash"
 ./aurevoir.bash
